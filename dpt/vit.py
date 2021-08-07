1ab4e81d91f6fcadf5d1dcb527800b2db5046869
Chris Ho <cho@Chris-MBP.local>
2021-08-06 17:19:22 -0700

    Unnest attention for multiprocessing

--- dpt/vit.py
+++ dpt/vit.py
@@ -7,38 +7,38 @@ import torch.nn.functional as F
 
 activations = {}
 
+def act_hook(model, input, output, name):
+    activations[name] = output
 
 def get_activation(name):
-    def hook(model, input, output):
-        activations[name] = output
-
-    return hook
+    return lambda x,y,z: act_hook(x,y,z,name)
 
 
 attention = {}
 
 
+def attn_hook(module, input, output, name):
+    x = input[0]
+    B, N, C = x.shape
+    qkv = (
+        module.qkv(x)
+        .reshape(B, N, 3, module.num_heads, C // module.num_heads)
+        .permute(2, 0, 3, 1, 4)
+    )
+    q, k, v = (
+        qkv[0],
+        qkv[1],
+        qkv[2],
+    )  # make torchscript happy (cannot use tensor as tuple)
+
+    attn = (q @ k.transpose(-2, -1)) * module.scale
+
+    attn = attn.softmax(dim=-1)  # [:,:,1,1:]
+    attention[name] = attn
+        
 def get_attention(name):
-    def hook(module, input, output):
-        x = input[0]
-        B, N, C = x.shape
-        qkv = (
-            module.qkv(x)
-            .reshape(B, N, 3, module.num_heads, C // module.num_heads)
-            .permute(2, 0, 3, 1, 4)
-        )
-        q, k, v = (
-            qkv[0],
-            qkv[1],
-            qkv[2],
-        )  # make torchscript happy (cannot use tensor as tuple)
 
-        attn = (q @ k.transpose(-2, -1)) * module.scale
-
-        attn = attn.softmax(dim=-1)  # [:,:,1,1:]
-        attention[name] = attn
-
-    return hook
+    return lambda x,y,z: attn_hook(x,y,z,name)
 
 
 def get_mean_attention_map(attn, token, shape):
