4eaaa4cb429a03043ba2aecfe60b99459f1ff0cd
Chris Ho <cho@Chris-MBP.local>
2021-08-28 11:30:54 -0700

    Removing k8s files

--- util/callbacks.py
+++ util/callbacks.py
@@ -11,13 +11,9 @@ class TensorCheckpoint(Callback):
         
     def on_validation_epoch_start(self, trainer, pl_module):
         if (self.every_n_epochs > 0 
-            and ((trainer.current_epoch + 1) % self.every_n_epochs == 0
+            and ((trainer.current_epoch + 1) % self.every_n_epochs) == 0 
             and trainer.is_global_zero):
             
             # save tensors here
-            print(trainer.log_dir)
-            print(pl_module.model)
-            
-#             path = os.path.join(trainer.log_dir, f'st-{trainer.current_epoch}')
-            
-#             torch.save({'s': pl_module.s, 't': pl_module.t}, path)
+            path = os.path.join(trainer.log_dir, f'{self.filename}-{trainer.current_epoch}.pt')
+            torch.save({'s': pl_module.s, 't': pl_module.t}, path)
